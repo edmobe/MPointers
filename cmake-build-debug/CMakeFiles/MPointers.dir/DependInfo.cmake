304920d9a0deb043cc4b79177edd44dfc14b5b87
edmobe <e.d.mobe@hotmail.com>
2018-09-28 01:22:44 -0600

    Add MPointerGC (gives ID, has a list of memory addresses).

---
+++ cmake-build-debug/CMakeFiles/MPointers.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/edmobe/TEC/Datos II/MPointers/MPointer/Client.cpp" "/home/edmobe/TEC/Datos II/MPointers/cmake-build-debug/CMakeFiles/MPointers.dir/MPointer/Client.cpp.o"
+  "/home/edmobe/TEC/Datos II/MPointers/MPointer/MPointer.cpp" "/home/edmobe/TEC/Datos II/MPointers/cmake-build-debug/CMakeFiles/MPointers.dir/MPointer/MPointer.cpp.o"
+  "/home/edmobe/TEC/Datos II/MPointers/MPointer/MPointerGC.cpp" "/home/edmobe/TEC/Datos II/MPointers/cmake-build-debug/CMakeFiles/MPointers.dir/MPointer/MPointerGC.cpp.o"
+  "/home/edmobe/TEC/Datos II/MPointers/MPointer/main.cpp" "/home/edmobe/TEC/Datos II/MPointers/cmake-build-debug/CMakeFiles/MPointers.dir/MPointer/main.cpp.o"
+  "/home/edmobe/TEC/Datos II/MPointers/MServer/AbstractDataType/DoublyLinkedList.cpp" "/home/edmobe/TEC/Datos II/MPointers/cmake-build-debug/CMakeFiles/MPointers.dir/MServer/AbstractDataType/DoublyLinkedList.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../MPointer"
+  "../MServer/AbstractDataType"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/MPointers.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MPointers.dir/MPointer/Client.cpp.o"
+  "CMakeFiles/MPointers.dir/MPointer/MPointer.cpp.o"
+  "CMakeFiles/MPointers.dir/MPointer/main.cpp.o"
+  "CMakeFiles/MPointers.dir/MServer/AbstractDataType/DoublyLinkedList.cpp.o"
+  "CMakeFiles/MPointers.dir/MPointer/MPointerGC.cpp.o"
+  "MPointers.pdb"
+  "MPointers"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MPointers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
